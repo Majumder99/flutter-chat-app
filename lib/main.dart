df8544b6a0ff3e0e0a130d6bb252250aa0452ac3
Sourav Majumder <souravmajumderk99@gmail.com>
2023-02-28 21:04:50 +0600

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,32 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: unused_import, prefer_const_constructors
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import "package:flutter/foundation.dart";
+import "shared/Constant.dart";
+import "pages/home_page.dart";
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constant.apiKey,
+            appId: Constant.appId,
+            messagingSenderId: Constant.messagingSenderId,
+            projectId: Constant.projectId));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Text("Hello"));
+        title: 'Chat App', debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: unused_import
+import "package:flutter/material.dart";
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
